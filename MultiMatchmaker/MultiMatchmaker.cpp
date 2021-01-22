385966bf800f4de926ed0fd42f4fdcce03a687d6
Axieof <pritheev24@gmail.com>
2021-01-22 10:26:58 +0800

    CSV Reader Function

--- MultiMatchmaker/MultiMatchmaker.cpp
+++ MultiMatchmaker/MultiMatchmaker.cpp
@@ -1,20 +1,43 @@
 // MultiMatchmaker.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Done by Caleb & Pritheev
+// DSA Assignment
 
+// Imports
 #include <iostream>
+#include <string>
+#include <fstream> // read and write to files
+#include <vector> // store the string
+#include <sstream>// stringstream
+using namespace std;
+
+vector<string> readRecordFromFile(string file_name)
+{
+    vector<string> record;
+
+    ifstream file;
+    file.open(file_name);
+
+    string name, matchCount, winRate, skillLevel;
+
+    while (getline(file, name))
+    {
+        getline(file, matchCount);
+        getline(file, winRate);
+        getline(file, skillLevel);
+
+        cout << name << endl;
+        cout << matchCount << endl;
+        cout << winRate << endl;
+        cout << skillLevel << endl;
+    }
+
+    return record;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";;
+    vector<string> Champion = readRecordFromFile("../Champions.csv");
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
